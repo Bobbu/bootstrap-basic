d75a0b0ad197581b88bf7cea9f38ece965edcab0
Rob Daly <robertjosephdaly@gmail.com>
2021-02-16 13:51:25 -0500

    Added comments to the importat files (index.html and app.component.ts)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+
+///////////////////////////////////////////////////////////////////////////////
+// The following are so that tooltips displatys on the template of this
+// component can take on the Bootstrap look and popper behavior. Repeat in as
+// many components as might need it.
 declare var $;
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+///////////////////////////////////////////////////////////////////////////////
 
 @Component({
   selector: 'app-root',
